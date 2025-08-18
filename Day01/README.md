d00f8f03a7099babc2c5cce51ad108eafdf0ef89
Sanika Surose <109695485+sanikasurose@users.noreply.github.com>
2025-08-17 22:59:16 -0400

    Add Day 1 project file and updated ReadMe

---
+++ Day01/README.md
@@ -0,0 +1,8 @@
+# Day 01 - Skills Learned
+
+## Skills Today
+
+- Print Statements
+- Strings
+- String Concatenation
+- Variables
---
+++ Day01/day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in? ")
+pet = input("What was the name of your childhood pet? ")
+bandname = city + " " + pet
+print("Your new band name is: " + bandname)
